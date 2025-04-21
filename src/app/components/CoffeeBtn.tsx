858ac9bf3cd434d1dd4382170c9cb52afa315b20
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-04-21 19:42:14 -0300

    fix: coffe btn link

--- src/app/components/CoffeeBtn.tsx
+++ src/app/components/CoffeeBtn.tsx
@@ -4,14 +4,16 @@ import { FaCoffee } from "react-icons/fa";
 export const CoffeeBtn = ({
     className,
     ...props
-}: HTMLAttributes<HTMLButtonElement>) => {
+}: HTMLAttributes<HTMLAnchorElement>) => {
     return (
-        <button
+        <a
             {...props}
+            href="https://ko-fi.com/zolppy"
+            target="_blank"
             className={`${className} p-4 rounded-full bg-[#f44336] text-white text-2xl active:bg-black active:text-white lg:hover:cursor-pointer lg:hover:bg-black lg:hover:text-white fixed bottom-[10px] right-[10px]`}
         >
             <FaCoffee />
-        </button>
+        </a>
     );
 };
 
